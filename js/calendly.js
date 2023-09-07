535cdea019ec653ca775ecb41423935452c14b30
Anupam Saraph <anupamsaraph@gmail.com>
2023-09-07 11:10:39 -0500

    Add Calendly Scheduler

---
+++ js/calendly.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    Calendly.initBadgeWidget({
+        url: 'https://calendly.com/anupamsaraph/focused-session?back=1',
+        text: 'Schedule time with Anupam Saraph',
+        color: '#4e97ff',
+        textColor: '#ffffff',
+        branding: false
+    });
+}
